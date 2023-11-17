48d003e2af20974af61ed448075f36ac3875168a
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-11-16 20:20:22 -0800

    Update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -35,6 +35,23 @@ namespace Prog_122_L15_ListView
             new Movie("The Five Heartbeats", "English", 1991, 121, "Robert Townsend")
         };
 
+            Movie straightOuttaCompton = movies[0];
+            CrewMember Lakeith = new CrewMember("LaKeith", "Stanfield", CrewMember.Role.Background);
+            straightOuttaCompton.AddCastMember(Lakeith);
+
+            Movie ironGiant = movies[1];
+            CrewMember bird = new CrewMember("Brad", "Bird", CrewMember.Role.Crew);
+            ironGiant.AddCastMember(bird);
+
+            Movie fiveHeartBeats = movies[2];
+            CrewMember townsend = new CrewMember("Robert", "Townsend", CrewMember.Role.Main);
+            fiveHeartBeats.AddCastMember(townsend);
+
+            // LaKeith Stanfield - Background
+            // Brad Bird - Crew
+            // Robert Townsend - Main Crew
+
+
             //lbDisplay.ItemsSource = movies;
 
             lvMovieInventory.ItemsSource = movies;
@@ -42,6 +59,24 @@ namespace Prog_122_L15_ListView
 
         }
 
+        private void btnAddMovie_Click(object sender, RoutedEventArgs e)
+        {
+            movies.Add(new Movie("Eternal Sunshine of a Spotless Mind", "English", 2001, 176, "Micheal Gondry"));
+
+            lvMovieInventory.Items.Refresh();
+        }
+
+        private void lvMovieInventory_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            int index = lvMovieInventory.SelectedIndex;
+            Movie movie = movies[index];
+            CrewMember firstMemeber = movie.Cast[0];
+
+            lvCrew.ItemsSource = movie.Cast;
+
+            //MessageBox.Show(firstMemeber.FName);
+        }
     } // class
 
     public class Movie
@@ -51,6 +86,9 @@ namespace Prog_122_L15_ListView
         int _year;
         int _duration;
         string _director;
+        List<CrewMember> _cast;
+
+        //List<> _cast;
 
         public Movie(string name, string captions, int year, int duration, string director)
         {
@@ -59,6 +97,7 @@ namespace Prog_122_L15_ListView
             _year = year;
             _duration = duration;
             _director = director;
+            _cast = new List<CrewMember>();
         }
 
         public string Name { get => _name; set => _name = value; }
@@ -66,13 +105,46 @@ namespace Prog_122_L15_ListView
         public int Year { get => _year; set => _year = value; }
         public int Duration { get => _duration; set => _duration = value; }
         public string Director { get => _director; set => _director = value; }
+        public List<CrewMember> Cast { get => _cast; set => _cast = value; }
+
         // cast
+
+        public void AddCastMember(string fname, string lname, CrewMember.Role role)
+        {
+            _cast.Add(new CrewMember(fname, lname, role));
+        }
+
+        public void AddCastMember(CrewMember crewMember)
+        {
+            _cast.Add(crewMember);
+        }
 
         public override string ToString()
         {
             return $"Name {Name} - Year {Year} - Director: {Director}";
         }
 
+    }
+
+    public class CrewMember
+    {
+        // enum
+        public enum Role { Main, Background, Crew, Uncredited }
+
+        string _fName;
+        string _lName;     
+        Role _role;
+
+        public string FName { get => _fName; set => _fName = value; }
+        public string LName { get => _lName; set => _lName = value; }
+        private Role Role1 { get => _role; set => _role = value; }
+
+        public CrewMember(string fName, string lName, Role role)
+        {
+            _fName = fName;
+            _lName = lName;
+            _role = role;
+        }
     }
 
 } // namespace
